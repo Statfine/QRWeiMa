3c087e19a8127ee09274c6b2a841243fcb86088d
Statfine <309814872@qq.com>
2015-08-12 13:55:21 +0800

    二维码
    二维码（生成，识别）

---
+++ src/com/sj/erweima/MainActivity.java
@@ -0,0 +1,208 @@
+package com.sj.erweima;
+
+
+import java.util.HashMap;
+import java.util.Hashtable;
+import java.util.Map;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.res.Resources.NotFoundException;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.BinaryBitmap;
+import com.google.zxing.ChecksumException;
+import com.google.zxing.DecodeHintType;
+import com.google.zxing.EncodeHintType;
+import com.google.zxing.FormatException;
+import com.google.zxing.Result;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.common.HybridBinarizer;
+import com.google.zxing.qrcode.QRCodeReader;
+import com.google.zxing.qrcode.QRCodeWriter;
+import com.sj.app.decoding.RGBLuminanceSource;
+
+public class MainActivity extends Activity {
+	private final String TAG = "MainActivity";
+	private final static int SCANNIN_GREQUEST_CODE = 1;
+	/**
+	 * 显示扫描结果
+	 */
+	private TextView mTextView ;
+	/**
+	 * 显示扫描拍的图片
+	 */
+	private ImageView mImageView;
+	
+	private EditText mEditText;
+	private Button mButton_set;
+	private Button mButton_get;
+	
+	private final int QR_WIDTH = 100;
+	private final int QR_HEIGHT = 100;
+	
+	private boolean flag = false;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		mTextView = (TextView) findViewById(R.id.result); 
+		mImageView = (ImageView) findViewById(R.id.qrcode_bitmap);
+		mEditText = (EditText) findViewById(R.id.et_qr);
+		mButton_set = (Button) findViewById(R.id.btn_set_qr);
+		mButton_get = (Button) findViewById(R.id.btn_get_qr);
+		
+		//点击按钮跳转到二维码扫描界面，这里用的是startActivityForResult跳转
+		//扫描完了之后调到该界面
+		Button mButton = (Button) findViewById(R.id.button1);
+		mButton.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent();
+				intent.setClass(MainActivity.this, MipcaActivityCapture.class);
+				intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+				startActivityForResult(intent, SCANNIN_GREQUEST_CODE);
+			}
+		});
+		
+		mButton_set.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				String text = mEditText.getText().toString();
+				if(text.length()> 0){
+					createImage();
+					flag = true;
+				}else{
+					Toast.makeText(MainActivity.this, "请输入内容", Toast.LENGTH_SHORT).show();
+					flag = false;
+				}
+				
+			}
+		});
+		
+		mButton_get.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				if(flag){
+					scanningImage();
+				}else{
+					Toast.makeText(MainActivity.this, "还未生成二维码", Toast.LENGTH_SHORT).show();
+				}
+			}
+		});
+	}
+	
+	
+
+	@Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+		case SCANNIN_GREQUEST_CODE:
+			if(resultCode == RESULT_OK){
+				Bundle bundle = data.getExtras();
+				//显示扫描到的内容
+				mTextView.setText(bundle.getString("result"));
+				//显示
+				mImageView.setImageBitmap((Bitmap) data.getParcelableExtra("bitmap"));
+				flag = true;
+			}
+			break;
+		}
+    }	
+	
+	
+	// 生成QR图
+    private void createImage() {
+        try {
+            // 需要引入core包
+            QRCodeWriter writer = new QRCodeWriter();
+
+            String text = mEditText.getText().toString();
+
+            Log.i(TAG, "生成的文本：" + text);
+            if (text == null || "".equals(text) || text.length() < 1) {
+                return;
+            }
+
+            // 把输入的文本转为二维码
+            BitMatrix martix = writer.encode(text, BarcodeFormat.QR_CODE,
+                    QR_WIDTH, QR_HEIGHT);
+
+            System.out.println("w:" + martix.getWidth() + "h:"
+                    + martix.getHeight());
+
+            Hashtable<EncodeHintType, String> hints = new Hashtable<EncodeHintType, String>();
+            hints.put(EncodeHintType.CHARACTER_SET, "utf-8");
+            BitMatrix bitMatrix = new QRCodeWriter().encode(text,
+                    BarcodeFormat.QR_CODE, QR_WIDTH, QR_HEIGHT, hints);
+            int[] pixels = new int[QR_WIDTH * QR_HEIGHT];
+            for (int y = 0; y < QR_HEIGHT; y++) {
+                for (int x = 0; x < QR_WIDTH; x++) {
+                    if (bitMatrix.get(x, y)) {
+                        pixels[y * QR_WIDTH + x] = 0xff000000;
+                    } else {
+                        pixels[y * QR_WIDTH + x] = 0xffffffff;
+                    }
+
+                }
+            }
+
+            Bitmap bitmap = Bitmap.createBitmap(QR_WIDTH, QR_HEIGHT,
+                    Bitmap.Config.ARGB_8888);
+
+            bitmap.setPixels(pixels, 0, QR_WIDTH, 0, 0, QR_WIDTH, QR_HEIGHT);
+            mImageView.setImageBitmap(bitmap);
+
+        } catch (WriterException e) {
+            e.printStackTrace();
+        }
+    }
+    
+ // 解析QR图片
+    private void scanningImage() {
+
+        Hashtable<DecodeHintType, String> hints = new Hashtable<DecodeHintType, String>();
+        hints.put(DecodeHintType.CHARACTER_SET, "utf-8");
+
+        // 获得待解析的图片
+        Bitmap bitmap = ((BitmapDrawable) mImageView.getDrawable()).getBitmap();
+        RGBLuminanceSource source = new RGBLuminanceSource(bitmap);
+        BinaryBitmap bitmap1 = new BinaryBitmap(new HybridBinarizer(source));
+        QRCodeReader reader = new QRCodeReader();
+        Result result;
+        try {
+            result = reader.decode(bitmap1, hints);
+            // 得到解析后的文字
+            mTextView.setText(result.getText());
+        } catch (NotFoundException e) {
+            e.printStackTrace();
+        } catch (ChecksumException e) {
+            e.printStackTrace();
+        } catch (FormatException e) {
+            e.printStackTrace();
+        } catch (com.google.zxing.NotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    }
+
+}
